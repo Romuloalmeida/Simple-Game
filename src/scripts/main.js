d7e5d26960dc7d3ad2bb91f380aeb850dd4bcbf1
Romulo <romulosousa17@gmail.com>
2020-03-18 19:09:25 -0300

    different monsters

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,7 @@
 const PIXI = require("pixi.js"),
-    { Text,Application, TextStyle, Container, Loader, Sprite, utils, Rectangle } = PIXI;
+    { Text,Application, TextStyle, Container, Loader, Sprite, utils, AnimatedSprite } = PIXI;
+
+
 
 const hitTestRectangle = require('./utils/hitTestRectangle').default;
 const contain = require("./utils/contain").default;
@@ -24,7 +26,7 @@ let TextureCache = utils.TextureCache
 //     name: 'explorerImage',
 //     url: 'images/explorer.png'
 //   }];
-const resources = "images/treasureHunter.json";
+const resources = ["images/treasureHunter.json","images/ninja.json","images/adventuress.json"];
 
 
 
@@ -34,7 +36,7 @@ loader.add(resources).load(setup).on("progress", function (loader, resource) {
     console.log("progress: " + loader.progress + "%");
 });
 
-let blobs,message,healthBar,gameOverScene,gameScene,state,dungeon, explorer, treasure, door, id;
+let gotTreasure,blobs,message,healthBar,gameOverScene,gameScene,state,dungeon, explorer, treasure, door, id;
 
 
 
@@ -60,12 +62,49 @@ function setup() {
     door.position.set(32, 0);
     gameScene.addChild(door);
 
-    let explorerTexture = TextureCache["explorer.png"];
-    explorer = new Sprite(explorerTexture);
+    let explorerTexture = loader.resources["images/adventuress.json"].spritesheet;
+    let explorerStates = {
+        down:[0],
+        left: [3],
+        right: [6],
+        up: [9],
+        walkDown: [0, 2],
+        walkLeft: [4, 6],
+        walkRight: [6, 8],
+        walkUp: [9, 11]
+    };
+
+
+    explorer = new AnimatedSprite(explorerTexture.animations["adventuress"]);
+    explorer.changeTexture = function(state){
+        var toChange = explorerTexture.animations["adventuress"].slice(state[0], state[1] ?? state[0] + 1);
+        this.textures = toChange;
+        this.play();
+    }
+    
+    explorer.animationSpeed = 30;
+    explorer.states = explorerStates;
+
+    explorer.changeTexture(explorerStates.down);
+    explorer.play();
+    
+
+    explorer.width = 30;
+    explorer.height = 40;
+
+
+
     explorer.x = 68;
 
     explorer.vx = 0;
     explorer.vy = 0;
+    explorer.accelerationX = 0; 
+    explorer.accelerationY = 0;
+    explorer.frictionX = 1;
+    explorer.frictionY = 1;
+    explorer.speed = 0.4;
+    explorer.maxSpeed = 4;
+    explorer.drag = 0.9;
 
     //Center the explorer vertically
     explorer.y = app.stage.height / 2 - explorer.height / 2;
@@ -109,7 +148,17 @@ function setup() {
         blob.x = x;
         blob.y = y;
 
-        blob.vy = 10;
+        blob.vy = 5;
+
+        if (randomInt(1,100) >= 80){
+            blob.tint = 0xE32727;
+            blob.dangerous = true;
+        }else{
+            if (randomInt(1,100) >= 80  ){
+                blob.tint = 0x09D639;
+                blob.life = true;
+            }
+        }
 
         //Add the blob sprite to the stage
         blobs.addChild(blob);
@@ -166,40 +215,69 @@ function setup() {
 
     //Left arrow key `press` method
     left.press = () => {
+        explorer.changeTexture(explorerStates.walkLeft);
         //Change the explorer's velocity when the key is pressed
-        explorer.vx = -5;
+        //explorer.vx = -5;
+        explorer.accelerationX = -explorer.speed; 
+        explorer.frictionX = 1;
     };
     
     //Left arrow key `release` method
     left.release = () => {
+        explorer.changeTexture(explorerStates.left);
         //If the left arrow has been released, and the right arrow isn't down,
         //and the explorer isn't moving vertically:
         //Stop the explorer
-        explorer.vx = 0;
+        // explorer.vx = 0;
+        explorer.accelerationX = 0; 
+        explorer.frictionX = explorer.drag;
+
     };
 
     //Up
     up.press = () => {
-        explorer.vy = -5;
+        explorer.changeTexture(explorerStates.walkUp);
+
+        // explorer.vy = -5;
+        explorer.accelerationY = -explorer.speed; 
+        explorer.frictionY = 1;
     };
     up.release = () => {
-        explorer.vy = 0;
+        explorer.changeTexture(explorerStates.up);
+        // explorer.vy = 0;
+        explorer.accelerationY = 0; 
+        explorer.frictionY = explorer.drag;
     };
 
     //Right
     right.press = () => {
-        explorer.vx = 5;
+        explorer.changeTexture(explorerStates.walkRight);
+
+        // explorer.vx = 5;
+        explorer.accelerationX = explorer.speed; 
+        explorer.frictionX = 1;
+        
     };
     right.release = () => {
-        explorer.vx = 0;
+        explorer.changeTexture(explorerStates.right);
+
+        // explorer.vx = 0;
+        explorer.accelerationX = 0; 
+        explorer.frictionX = explorer.drag;
     };
 
     //Down
     down.press = () => {
-        explorer.vy = 5;
+        explorer.changeTexture(explorerStates.walkDown);
+        // explorer.vy = 5;
+        explorer.accelerationY = explorer.speed; 
+        explorer.frictionY = 1;
     };
     down.release = () => {
-        explorer.vy = 0;
+        explorer.changeTexture(explorerStates.down);
+        // explorer.vy = 0;
+        explorer.accelerationY = 0; 
+        explorer.frictionY = explorer.drag;
     };
 
 }
@@ -212,9 +290,45 @@ function gameLoop(delta){
 // All here is 60fps
 function play(delta) {
 
-    if(!contain(explorer, {x: 32, y: 0, width: 488, height: 480})){
-        explorer.x += explorer.vx;
-        explorer.y += explorer.vy;
+    //Apply acceleration by adding the acceleration
+    //to the sprite's velocity
+    if (Math.abs(explorer.vx) + Math.abs(explorer.accelerationX)  <= explorer.maxSpeed){
+        explorer.vx += explorer.accelerationX;
+    }
+    if (Math.abs(explorer.vy) + Math.abs(explorer.accelerationY)  <= explorer.maxSpeed){
+        explorer.vy += explorer.accelerationY;
+        //Apply friction by multiplying sprite's velocity by the friction
+    }
+    
+    //Apply friction by multiplying sprite's velocity by the friction
+    explorer.vx *= explorer.frictionX;
+    explorer.vy *= explorer.frictionY;
+    
+
+    //Add optional gravity
+    //explorer.vy += 0.1;
+
+    //Apply the velocity to the sprite's position to
+    //make it move
+    explorer.x += explorer.vx;
+    explorer.y += explorer.vy;
+
+    
+
+    var collision = contain(explorer, //The sprite you want to contain
+        {x: 32, y: 0, width: 488, height: 480}
+    );
+
+    //Check for a collision. If the value of `collision` isn't
+    //`undefined` then you know the sprite hit a boundary 
+    if (collision) {
+
+        //Reverse the sprite's `vx` value if it hits the left or right
+        if (collision.has("left") || collision.has("right")) explorer.vx = -explorer.vx * 0.7;
+
+        //Reverse the sprite's `vy` value if it hits the top or bottom
+        if (collision.has("top") || collision.has("bottom")) explorer.vy = -explorer.vy * 0.7;
+
     }
     
 
@@ -231,14 +345,23 @@ function play(delta) {
       
         //If the blob hits the top or bottom of the stage, reverse
         //its direction
-        if (blobHitsWall === "top" || blobHitsWall === "bottom") {
+        if (blobHitsWall != undefined ) {
           blob.vy *= -1;
         }
-      
+        
         //Test for a collision. If any of the enemies are touching
         //the explorer, set `explorerHit` to `true`
         if(hitTestRectangle(explorer, blob)) {
           explorerHit = true;
+          
+          if (blob.dangerous == true){
+            //Reduce the width of the health bar's inner rectangle by 1 pixel
+            healthBar.outer.width -= 50;
+          }else if (blob.life == true){
+            healthBar.outer.width += 50;
+            blob.destroy();
+          }
+
         }
         
       });
@@ -255,9 +378,13 @@ function play(delta) {
         explorer.alpha = 1;
       }
 
-      if (hitTestRectangle(explorer, treasure)) {
+      if (hitTestRectangle(explorer, treasure)){
+        gotTreasure = true;
+      }
+
+      if (gotTreasure == true){
         treasure.x = explorer.x + 8;
-        treasure.y = explorer.y + 8;
+        treasure.y = explorer.y + 20;
       }
 
       if (hitTestRectangle(treasure, door)) {
@@ -275,6 +402,7 @@ function play(delta) {
 function end(){
     gameScene.visible = false;
     gameOverScene.visible = true;
+
 }
 
   
--- src/scripts/utils/contain.js
+++ src/scripts/utils/contain.js
@@ -1,31 +1,36 @@
 exports.default = function contain(sprite, container) {
 
-    let collision = undefined;
-  
-    //Left
-    if (sprite.x < container.x) {
-      sprite.x = container.x;
-      collision = "left";
-    }
-  
-    //Top
-    if (sprite.y < container.y) {
-      sprite.y = container.y;
-      collision = "top";
-    }
-  
-    //Right
-    if (sprite.x + sprite.width > container.width) {
-      sprite.x = container.width - sprite.width;
-      collision = "right";
-    }
-  
-    //Bottom
-    if (sprite.y + sprite.height > container.height) {
-      sprite.y = container.height - sprite.height;
-      collision = "bottom";
-    }
-  
-    //Return the `collision` value
-    return collision;
+  //Create a set called `collision` to keep track of the
+  //boundaries with which the sprite is colliding
+  var collision = new Set();
+
+  //Left
+  if (sprite.x < container.x) {
+    sprite.x = container.x;
+    collision.add("left");
   }
+
+  //Top
+  if (sprite.y < container.y) {
+    sprite.y = container.y;
+    collision.add("top");
+  }
+
+  //Right
+  if (sprite.x + sprite.width > container.width) {
+    sprite.x = container.width - sprite.width;
+    collision.add("right");
+  }
+
+  //Bottom
+  if (sprite.y + sprite.height > container.height) {
+    sprite.y = container.height - sprite.height;
+    collision.add("bottom");
+  }
+
+  //If there were no collisions, set `collision` to `undefined`
+  if (collision.size === 0) collision = undefined;
+
+  //Return the `collision` value
+  return collision;
+}
